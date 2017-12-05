a0de9e2d7a0906b8d8a6840a7005c18597eadc3f
BalintCsala <33815391+BalintCsala@users.noreply.github.com>
2017-12-05 01:13:46 +0100

    Add files via upload

--- JS1k/main másolata.js
+++ JS1k/main másolata.js
@@ -4,52 +4,55 @@ f = r => {
            I = s(1.5), 
            u = v = 32, 
            p = s(Q=3),
-           C = (a, b) => s(a.x + b.x, a.y + b.y), 
            o = (a, b) => s(a.x / b, a.y / b),
            h = (p, i = 1) => [s(-2, 0), s(2, 0), s(0, -2), s(0, 2)].filter(e => g(C(p, e)) == i), 
            t = p => s(Math.cos(p), Math.sin(p)), 
            g = p => (M[p.y | 0] || 0)[p.x | 0] | 0, 
            S = (a, b) => M[a.y | 0][a.x | 0] = b; 
            Q&&S(p,2);) {
-        if (!(A = h(p))[U = E = L = 0] || !(l = C(p, A[Math.random() * A.length | 0]))) {
+        if (!(A = h(p))[B=U = E = L = 0] || !(l = C(p, A[Math.random() * A.length | 0]))) {
             for (x = 0; x < u; x++)
-                for (y = 0; y < v; y++) g(Z = s(x + 1, y + 1)) - 1 || !(J = h(D = Z, 2)[0]) || (p = C(J, l = D), Q = x = y = u);
+                for (y = 0; y < v; y++) 
+                    g(Z = s(x + 1, y + 1)) - 1 || (J = h(D = Z, 2)[0]) && (p = C(J, l = D), Q = x = y = u);
             Q--
         }
         S(o(C(p, p = l), 2), 2)
     }
     u = 1, v = 5;
     for (x = 0; x < u; x++)
-        for (y = 0; y < v; y++) S(s(Math.random() * 32 | 0 + 1, Math.random() * 32 | 0 + 1), 3)
+        for (y = 0; y < v; y++) 
+            S(s(Math.random() * 32 | 0 + 1, Math.random() * 32 | 0 + 1), 3)
 };
 f();
 u = "onkeydown";
 d[u] = e => k[e.keyCode] = 1;
 u = "onkeyup";
 d[u] = e => k[e.keyCode] = 0;
-(k = _ => {
-    g(I) - 3 || (S(I, 2), E++);
-    d = o(t(L), 396 / (-U + (U = _)));
-    T = 5e-2;
+(Y = _ => {
+    g(I) - 3 || S(I, 2) | E++;
     i = 36;
-    k[++i] && (L += -T);
-    k[++i] && g(n = C(I, d)) && (I = n);
-    k[++i] && (L += T);
-    d = o(d, -1);
-    k[++i] && g(n = C(I, d)) && (I = n);
-    I.x + I.y < 62 || !E || E % 5 || f();
-    c.fillStyle = "#24f";
-    c.fillRect(0, 0, u = 400, u);
-    c.fillStyle = "#333";
-    c.fillRect(0, h = 200, u, h);
+    k[++i] && L--;
+    k[++i] && B++;
+    k[++i] && L++;
+    k[++i] && B--;
+    g(n = C(I, o(t(L/20),396 / ((U = _) - U) / (B /= 1.6)))) && (I = n);
+    I.x + I.y < 62 || E && E % 5 || f();
+    P=c.createLinearGradient(0,0,1,u=400);
+    P.addColorStop(0, "#24f");
+    P.addColorStop(.5,V="#fff");
+    P.addColorStop(1,"#333");
+    c.fillStyle = P;
+    c.fillRect(0, 0, u, u);
     v = 1e4;
     for (x = 0; x < u; x++)
-        for (y = 0; y < v; y++) w = C(I, o(t(L + (z = (x - u / 2) / u)), 150 / y * Math.cos(z))), g(w) && g(w) ^ 3 || (c.fillStyle = ["#999", "#bbb", "#ddd"][(w.x % 3 + w.y) % 3 | 0], c.fillRect(x, h - u / y / 1e-2 / 2, 2, u / y / 1e-2), y = v, x++);
+        for (y = 0; y < v; y++) 
+            w = C(I, o(t(L/20 + (z = (x - u / 2) / u)), 150 / y * Math.cos(z))), g(w) && g(w) ^ 3 || (c.fillStyle = `rgb(${[0,0,0].fill(25 * (7+(w.x + w.y) % 3 | 0) + y / 30 | 0)}`, c.fillStyle = g(w) - 3 || "#0f0", c.fillRect(x, 200 - u / y / .01 / 2, 1, u/y/.01), y = v);
     u = v = 33;
     for (x = 0; x < u; x++)
-        for (y = 0; y < v; y++) c.fillStyle = ["#000", 0, "#fff", "#0f0"][M[y][x]], c.fillRect(x * 3, y * 3, 3, 3);
+        for (y = 0; y < v; y++) 
+            c.fillStyle = ["#000", 0, V, "#0f0"][M[y][x]], c.fillRect(x * 3, y * 3, 3, 3);
     c.fillStyle = "#f00";
     c.fillRect(I.x * 3 - 1, I.y * 3 - 1, 3, 3);
     c.fillRect(93, 93, 3, 3);
-    requestAnimationFrame(k)
+    requestAnimationFrame(Y)
 })()
--- JS1k/main.js
+++ JS1k/main.js
@@ -4,6 +4,7 @@ b = document.body;
 c = a.getContext("2d");
 d = document;
 
+onkeyup = onkeydown = x => k[x.which] = x.type[5];
 
 f = r => {
     for (s = (a, b = a) => ({ x: a, y: b }), 
@@ -13,27 +14,24 @@ f = r => {
            p = s(Q=3),
            C = (a, b) => s(a.x + b.x, a.y + b.y), 
            o = (a, b) => s(a.x / b, a.y / b),
-           h = (p, i = 1) => [s(-2, 0), s(2, 0), s(0, -2), s(0, 2)].filter(e => g(C(p, e)) == i), 
            t = p => s(Math.cos(p), Math.sin(p)), 
            g = p => (M[p.y | 0] || 0)[p.x | 0] | 0, 
            S = (a, b) => M[a.y | 0][a.x | 0] = b; 
-           Q&&S(p,2);) {
-        if (!(A = h(p))[B=U = E = L = 0] || !(l = C(p, A[Math.random() * A.length | 0]))) {
+           Q&S(p,2);) {
+        if (!(A = [s(-2, 0), s(2, 0), s(0, -2), s(0, 2)].filter(e => g(C(p, e)) == 1))[B = U = E = L = 0] || !(l = C(p, A[Math.random() * A.length | 0]))) {
             for (x = 0; x < u; x++)
-                for (y = 0; y < v; y++) g(Z = s(x + 1, y + 1)) - 1 || (J = h(D = Z, 2)[0]) && (p = C(J, l = D), Q = x = y = u);
+                for (y = 0; y < v; y++) 
+                    g(Z = s(x + 1, y + 1)) - 1 || (J = [s(-2, 0), s(2, 0), s(0, -2), s(0, 2)].filter(e => g(C(D=Z, e)) == 2)[0]) && (p = C(J, l = D), Q = x = y = u);
             Q--
         }
         S(o(C(p, p = l), 2), 2)
     }
-    u = 1, v = 5;
+    u=1,v = 5;
     for (x = 0; x < u; x++)
-        for (y = 0; y < v; y++) S(s(Math.random() * 32 | 0 + 1, Math.random() * 32 | 0 + 1), 3)
-};
-f();
-u = "onkeydown";
-d[u] = e => k[e.keyCode] = 1;
-u = "onkeyup";
-d[u] = e => k[e.keyCode] = 0;
+        for (y = 0; y < v; y++) 
+            S(s(Math.random() * 32 | 0 + 1, Math.random() * 32 | 0 + 1), 3)
+};f();
+
 (Y = _ => {
     g(I) - 3 || S(I, 2) | E++;
     i = 36;
@@ -41,26 +39,22 @@ d[u] = e => k[e.keyCode] = 0;
     k[++i] && B++;
     k[++i] && L++;
     k[++i] && B--;
-    B /= 1.6;
-    g(n = C(I, o(t(L/20),396 / (-U + (U = _))*1/B))) && (I = n);
+    g(n = C(I, o(t(L/20),396 / (U - (U = -_)) / (B/=1.6)))) && (I = n);
     I.x + I.y < 62 || E && E % 5 || f();
-    P=c.createLinearGradient(0,0,1,u=400);
+    c.fillStyle=P=c.createLinearGradient(0,0,0,u=400);
     P.addColorStop(0, "#24f");
-    P.addColorStop(.6,"#fff");
-    c.fillStyle = P;
-    c.fillRect(0, 0, u, u);
-    P=c.createLinearGradient(0,0,1,u=400);
-    P.addColorStop(.4, "#fff");
+    P.addColorStop(.5,V="#fff");
     P.addColorStop(1,"#333");
-    c.fillStyle = P;
-    c.fillRect(0, h = 200, u, h);
+    c.fillRect(0, 0, u, u);
     v = 1e4;
     for (x = 0; x < u; x++)
-        for (y = 0; y < v; y++) w = C(I, o(t(L/20 + (z = (x - u / 2) / u)), 150 / y * Math.cos(z))), g(w) && g(w) ^ 3 || (c.fillStyle = `hsl(0,0%,${[60, 70, 80][(w.x + w.y) % 3 | 0]+y/100}%`, c.fillStyle = g(w) - 3 || "#0f0", c.fillRect(x, h - u / y / .01 / 2, 1, u/y/.01), y = v);
+        for (y = 0; y < v; y++) 
+            w = C(I, o(t(L/20 + (z = x / u - .5)), 150 / y * Math.cos(z))), g(w)%3 || (c.fillStyle = g(w) - 3 ? "rgb("+Array(3).fill(175 + 25 * ((w.x + w.y) % 3 | 0) + y / 30 | 0) : "#0f0", c.fillRect(x, 200 - u / y / .01 / 2, 1, u/y/.01), y = v);
     u = v = 33;
     for (x = 0; x < u; x++)
-        for (y = 0; y < v; y++) c.fillStyle = ["#000", 0, "#fff", "#0f0"][M[y][x]], c.fillRect(x * 3, y * 3, 3, 3);
-    c.fillStyle = "#f00";
+        for (y = 0; y < v; y++)
+            c.fillStyle = ["#000", , V, "#0f0"][M[y][x]], c.fillRect(x * 3, y * 3, 3, 3);
+    c.fillStyle = "red";
     c.fillRect(I.x * 3 - 1, I.y * 3 - 1, 3, 3);
     c.fillRect(93, 93, 3, 3);
     requestAnimationFrame(Y)
