77f198c7cc55b1b3e016e38b3687b07dbfed67fe
BalintCsala <33815391+BalintCsala@users.noreply.github.com>
2018-02-10 01:22:20 +0100

    Add files via upload

--- js1k_2/main.js
+++ js1k_2/main.js
@@ -3,66 +3,61 @@ a = d.getElementById("canvas");
 b = d.body;
 c = a.getContext("2d");
 
-
-w = 0.08;
-v = 3
+w = a.width;
+h = a.height;
 p = [0, 0];
 e = [];
 s = [];
-for (i = 0; i < 200; i++)
-    s.push([(Math.random() - .5) * a.width, (Math.random() - .5) * a.height, Math.random() * 2 + 1])
-a.onmousemove = e => p = [e.clientX - a.offsetLeft - a.width / 2, e.clientY - a.offsetTop - a.height / 2];
+for (i = 0; i < 200; i++) s.push([(Math.random() - .5) * w, (Math.random() - .5) * h, Math.random() * 2 + 1]);
 
-a.style.backgroundColor = "black";
-c.translate(a.width / 2, a.height / 2);
+a.onmousemove = e => p = [e.clientX - w / 2, e.clientY - h / 2];
+
+a.style.backgroundColor = "#000";
 
 (l = t => {
-    c.clearRect(-a.width / 2, -a.height / 2, a.width, a.height);
+    a.width |= 0;
+    c.translate(w / 2, h / 2);
 
-    c.fillStyle = "white";
+    c.fillStyle = "#fff";
     s.map(x => c.fillRect(x[0], x[1], x[2], x[2]));
 
-    for (z = v - (t / 5000) % w; z > w; z-= w)
-        for (x = -1; x <= 1; x += w) {
-            c.beginPath();
-            c.lineWidth = 1;
-            c.lineTo(x * a.width / 2 / z, (a.height / 6 + Math.sin(x ** 2) * 60 + z ** 2 * 50 - 10 / Math.hypot(Math.abs(x) - 0.6, ((z + t / 5000) % 1 + 1) % 1 - .5)) / z);
-            c.lineTo((x + w) * a.width / 2 / z, (a.height / 6 + Math.sin((x + w) ** 2) * 60 + z ** 2 * 50 - 10 / Math.hypot(Math.abs(x + w) - 0.6, ((z + t / 5000) % 1 + 1) % 1 - .5)) / z);
-            c.lineTo((x + w) * a.width / 2 / (z - w), (a.height / 6 + Math.sin((x + w) ** 2) * 60 + (z - w) ** 2 * 50 - 10 / Math.hypot(Math.abs(x + w) - 0.6, ((z - w + t / 5000) % 1 + 1) % 1 - .5)) / (z - w));
-            c.lineTo(x * a.width / 2 / (z - w), (a.height / 6 + Math.sin(x ** 2) * 60 + (z - w) ** 2 * 50 - 10 / Math.hypot(Math.abs(x) - 0.6, ((z - w + t / 5000) % 1 + 1) % 1 - .5)) / (z - w));
-            c.lineTo(x * a.width / 2 / z, (a.height / 6 + Math.sin(x ** 2) * 60 + z ** 2 * 50 - 10 / Math.hypot(Math.abs(x) - 0.6, ((z + t / 5000) % 1 + 1) % 1 - .5)) / z);
-            c.fillStyle = `hsl(200, 100%, ${(x+1)*30+30}%)`
-            c.fill();
-            c.strokeStyle = "black"
+    for (z = 3 - (t / 5000) % 0.08; z > 0.08; z-= 0.08)
+        for (x = -1; x < 1; x += 0.08)
+            c.beginPath(),
+            [[0, 0], [0.08, 0], [0.08, 0.08], [0, 0.08], [0, 0]].map(m => c.lineTo((x + m[0]) * w / 2 / (z + m[1]), (h / 6 + Math.sin(x + m[0]) ** 2 * 60 + (z + m[1]) ** 2 * 50 - 10 / Math.hypot(Math.abs(x + m[0]) - 0.6, (z + m[1] + t / 5000) % 1 - .5)) / (z + m[1]))),
+            c.fillStyle = `hsl(200, 100%, ${(x+1)*30+30}%`,
+            c.fill(),
             c.stroke();
-        }
 
     u = [];
 
-    if (Math.random() < 0.04)
-        e.push([(Math.random() - .5) * a.width, (Math.random() - .5) * a.height, 10]);
+
+    Math.random() < 0.12 && e.push([(Math.random() - .5) * w, (Math.random() - .5) * h, 20]);
 
     e = e.filter(x => x[2] > 0.3);
-    e.map(x => (u.push([x[0] - 100, x[1] - 100, 200, x[2], "brown"]), x[2] -= 0.05));
+    e.map(x => (u.push([x[0] - 100, x[1] - 100, x[2], 200, `hsl(${30 - x[2]}, 100%, 20%`]), x[2] -= 0.2));
 
-    for (i = 0; i < 20; i++)
-        u.push([p[0] - 20 + i / 2, p[1] - 20 + i / 2, 40 - i, i / 40 + 1, "white"]);
+    for (i = 0; i < 20; i++) u.push([p[0] - 20 + i / 2, p[1] - 20 + i / 2, i / 40 + 1, 40 - i, "#fff"]);
 
-    [[-100, -50], [100, 50], [-100, 50], [100, -50]].map(x => {for(i = 0; i < 5; i++) u.push([p[0] + x[0] - 10, p[1] + x[1] - 10, 20, ((i + 2) / 40 + 1), "white"])})
+    [[-100, -50], [100, 50], [-100, 50], [100, -50]].map(x => {for(i = 2; i < 7; i++) u.push([p[0] + x[0] - 10, p[1] + x[1] - 10, (i / 40 + 1), 20, "#fff"])})
 
-    u.sort((a, b) => b[3] - a[3]).map(x => (c.fillStyle = x[4], c.fillRect(x[0] / x[3], x[1] / x[3], x[2] / x[3], x[2] / x[3])));
+    u.sort((x, b) => b[2] - x[2]).map(x => (c.fillStyle = x[4], c.fillRect(x[0] / x[2], x[1] / x[2], x[3] / x[2], x[3] / x[2])));
 
-    c.strokeStyle = "white";
+    c.strokeStyle = "#fff";
     c.lineWidth = 20;
+
     i = 2
     c.beginPath();
-    c.lineTo((p[0] - 100) / (i / 40 + 1), (p[1] - 50) / (i / 40 + 1));
+    c.moveTo((p[0] - 100) / (i / 40 + 1), (p[1] - 50) / (i / 40 + 1));
     c.lineTo((p[0] + 100) / (i / 40 + 1), (p[1] + 50) / (i / 40 + 1));
-    c.stroke();
-    c.beginPath();
-    c.lineTo((p[0] - 100) / (i / 40 + 1), (p[1] + 50) / (i / 40 + 1));
+    c.moveTo((p[0] - 100) / (i / 40 + 1), (p[1] + 50) / (i / 40 + 1));
     c.lineTo((p[0] + 100) / (i / 40 + 1), (p[1] - 50) / (i / 40 + 1));
     c.stroke();
 
-    requestAnimationFrame(l)
+
+    c.fillStyle = "#fff";
+    c.font = "30px Arial";
+    c.fillText((t / 1000).toFixed(1) + `s`, -w / 2 + 10, -h / 2 + 30)
+
+    e.some(x => x[2] < 1.5 && Math.hypot(p[0] - x[0], p[1] - x[1]) < 100) || requestAnimationFrame(l)
 })(0)
