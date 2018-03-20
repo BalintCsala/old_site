e555918f6d143d3f7a947206ed9249a09c981abc
Bálint Csala <balintcsala@gmail.com>
2018-03-20 21:15:07 +0100

    update

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@ let face = (function() {
     navigator.mediaDevices.getUserMedia({ video: true }).then(stream => {
 
         test.src = stream.url;
+        test.onloadedmetadata = function(e) {
+            video.play();
+        };
     });
 
 }());
