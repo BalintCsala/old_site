8286c02fee7c5075257d0dbe5574504778f53219
BalintCsala <33815391+BalintCsala@users.noreply.github.com>
2018-07-07 21:59:26 +0200

    Update main.js

--- raymarch/main.js
+++ raymarch/main.js
@@ -229,28 +229,30 @@
     }
 
     function update(t) {
-        if (keys[87] || keys[90] || keys[38]) { // Forward
-             cameraPosition[2] += Math.cos(cameraRotation[1]) * CAMERA_SPEED;
-             cameraPosition[0] += Math.sin(cameraRotation[1]) * CAMERA_SPEED;
-         }	         }
--        if (keys[83]) {	+        if (keys[83] || keys[40]) { // Backward
-             cameraPosition[2] -= Math.cos(cameraRotation[1]) * CAMERA_SPEED;
-             cameraPosition[0] -= Math.sin(cameraRotation[1]) * CAMERA_SPEED;
-         }	         }
--        if (keys[65]) {	+        if (keys[65] || keys[81] || keys[37]) { // Left
-             cameraPosition[2] -= Math.cos(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
-             cameraPosition[0] -= Math.sin(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
-         }	         }
--        if (keys[68]) {	+        if (keys[68] || keys[39]) { // Right
-             cameraPosition[2] += Math.cos(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
-             cameraPosition[0] += Math.sin(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
-        if (keys[32]) {
-            cameraPosition[1] += CAMERA_SPEED;
-        }
-        if (keys[16]) {
-            cameraPosition[1] -= CAMERA_SPEED;
-        }
         if (mouse) {
+            if (keys[87] || keys[90] || keys[38]) { // Forward
+                cameraPosition[2] += Math.cos(cameraRotation[1]) * CAMERA_SPEED;
+                cameraPosition[0] += Math.sin(cameraRotation[1]) * CAMERA_SPEED;
+            }
+    -       if (keys[83] || keys[40]) { // Backward
+                cameraPosition[2] -= Math.cos(cameraRotation[1]) * CAMERA_SPEED;
+                cameraPosition[0] -= Math.sin(cameraRotation[1]) * CAMERA_SPEED;
+            }
+    -       if (keys[65] || keys[81] || keys[37]) { // Left
+                cameraPosition[2] -= Math.cos(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
+                cameraPosition[0] -= Math.sin(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
+            }
+    -       if (keys[68] || keys[39]) { // Right
+                 cameraPosition[2] += Math.cos(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
+                 cameraPosition[0] += Math.sin(cameraRotation[1] + Math.PI / 2) * CAMERA_SPEED;
+            }
+            if (keys[32]) {
+                cameraPosition[1] += CAMERA_SPEED;
+            }
+            if (keys[16]) {
+                cameraPosition[1] -= CAMERA_SPEED;
+            }
+            
             cameraRotation[0] -= mouseDelta[1] * CAMERA_ANGULAR_SPEED;
             cameraRotation[1] += mouseDelta[0] * CAMERA_ANGULAR_SPEED;
             cameraRotation[0] = Math.max(Math.min(cameraRotation[0], Math.PI / 2), -Math.PI / 2)
